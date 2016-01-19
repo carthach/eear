af30f02e0b0756718d84d18c5c41883c3ce01658
Cárthach Ó Nuanáin <carthach@carthach.tk>
2016-01-19 17:40:24 +0100

    Working iOS version (does't work in simulator though)

--- eear-Mobile/Builds/iOS/dependencies/essentia/include/essentia/version.h
+++ eear-Mobile/Builds/iOS/dependencies/essentia/include/essentia/version.h
@@ -2,5 +2,5 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 #define ESSENTIA_VERSION "2.1-dev"
-#define ESSENTIA_GIT_SHA "v2.1_beta2-349-g744b28b-dirty"
+#define ESSENTIA_GIT_SHA "v2.1_beta2-363-g8e922a9-dirty"
 #endif /* VERSION_H_ */
